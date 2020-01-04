2332bf4f247d34f8a84c23a252fed5ac6c6db9eb
OurLearningProject <frasel271@gmail.com>
2020-01-04 14:02:51 +0600

    js overly for test

---
+++ js/myScript.js
@@ -0,0 +1,42 @@
+
+var btnMenu = document.getElementById("btn-menu");
+btnMenu.addEventListener("click",toggleMenu);
+
+function toggleMenu(e){
+
+	var divOverly = document.getElementsByClassName("overly")[0];
+	var speed = 10;
+
+	var eventTarget = e.target;
+
+	if( eventTarget.className == "btn-open"){
+		fadeIn(divOverly,speed);
+		eventTarget.className = "btn-close";
+	}
+	else if(eventTarget.className == "btn-close"){
+		fadeOut(divOverly,speed);
+		eventTarget.className = "btn-open";
+	}
+}
+
+function fadeIn(elem,speed){
+	var inInterval = setInterval(function(){
+		elem.style.opacity = Number(elem.style.opacity) + 0.02;
+
+		if (elem.style.opacity >=1){
+			elem.style.opacity = 1;
+			clearInterval(inInterval);
+		}
+	},speed);
+}
+
+function fadeOut(elem,speed){
+	var outInterval = setInterval(function(){
+		elem.style.opacity = Number(elem.style.opacity) - 0.02;
+
+		if (elem.style.opacity <=0){
+			elem.style.opacity = 0;
+			clearInterval(outInterval);
+		}
+	},speed);
+}
